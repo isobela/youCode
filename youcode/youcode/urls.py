8707e3dc86742cd929ef74b40106b23601e7b4b9
Isobel <isobeladamsiga@gmail.com>
2024-04-06 17:18:07 -0600

    more login stuff

--- youcode/youcode/urls.py
+++ youcode/youcode/urls.py
@@ -18,10 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from  applet import views
 from django.urls import path, include  # new
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.my_html_view),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"), 
     path("accounts/", include("django.contrib.auth.urls"))
 ]
 
