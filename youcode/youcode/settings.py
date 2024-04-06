c52f621aec6902414155f1e4b6e7d1f53887f325
elise-green <greeneliseb@gmail.com>
2024-04-06 16:45:31 -0700

    added a urls.py to applet

--- youcode/youcode/settings.py
+++ youcode/youcode/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'applet'
+    'applet',
+
 ]
 
 MIDDLEWARE = [
@@ -123,4 +124,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- youcode/youcode/urls.py
+++ youcode/youcode/urls.py
@@ -23,7 +23,8 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", TemplateView.as_view(template_name="home.html"), name="home"), 
-    path("accounts/", include("django.contrib.auth.urls"))
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("applet.urls")),
 ]
 
 
