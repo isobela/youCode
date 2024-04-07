cafb303f711871a9ec7b9525470926f6e80f806c
Isobel <isobeladamsiga@gmail.com>
2024-04-06 18:12:56 -0600

    signup page

--- youcode/applet/views.py
+++ youcode/applet/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
-def my_html_view(request):
-    return render(request, 'home.html')
+
+# accounts/views.py
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
