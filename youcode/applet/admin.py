7308b4d1954a96303b17c820d69d3dc3d47e5449
Isobel <isobeladamsiga@gmail.com>
2024-04-07 10:16:06 -0600

    merge

--- youcode/applet/admin.py
+++ youcode/applet/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Prize
+from .models import Prize , People, Event
 
 admin.site.register(Prize)
+
+admin.site.register(Event)
+
+admin.site.register(People)
 
