c0ba852ee742f608d392d3615a8aaa49fe758ff0
Isobel <isobeladamsiga@gmail.com>
2024-04-06 21:20:44 -0600

    prizes

--- youcode/applet/forms.py
+++ youcode/applet/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from .models import Event
+from .models import Prize
 
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ['event_name', 'location', 'description',  'image', 'coins']
+
